1aaba10f43477d30bede07b2553723892b9ba0ca
chukwudumebiughonu <plasdexcel14@gmail.com>
2024-06-19 14:26:13 +0100

    update shopProductionSection

--- src/components/sections/shop/ShopProductSection.tsx
+++ src/components/sections/shop/ShopProductSection.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import ProductCard from "@/components/cards/ProductCard";
+import { PRODUCTS } from "@/lib/constants";
 
 function ShopProductSection() {
   return (
-    <div>ShopProductSection</div>
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-[32px] mt-[46px]">
+        {PRODUCTS.map((item, index) => (
+          <ProductCard {...item} key={index} />
+        ))}
+      </div>
   )
 }
 
